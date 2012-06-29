00b334cb1034814b75b75cb86556783f89253b90
Jeff Sacks <jrsacks@gmail.com>
2012-06-29 15:29:56 -0500

    added results

--- current.rb
+++ current.rb
@@ -3,16 +3,29 @@
 require 'nokogiri'
 require 'open-uri'
 
-doc = Nokogiri::HTML(open("http://wimbledon.com/en_GB/scores/index2.html"))
-doc.css('.cmatch').each do |div|
-  if div.css('.eventinfo').first.inner_html.include? "Gentlemen's Singles"
-    div.css('.crtrow').each do |player|
-      points = player.css('.crtpts').search('span').text
-      sets = player.css('.crtset').map do |set|
-        set.search('span').text
+def find_day
+  open("http://www.wimbledon.com/en_GB/scores/results/index.html").read.match(/day[0-9]*/)[0]
+end
+
+def output_page(url)
+  doc = Nokogiri::HTML(open(url))
+  doc.css('.scoringtable').each do |div|
+    if div.css('.eventinfo').first.inner_html.include? "Gentlemen's Singles"
+      div.css('.crtrow').each do |player|
+        server = ''
+        server = '-' if player.css('.currentServer').length > 0
+        points = player.css('.crtpts').search('span').text
+        sets = player.css('.crtset').map do |set|
+          set.search('span').text
+        end
+        printf "%-20s  %-1s%-2s  %s\n", player.search('a').inner_html, server, points, sets.join(' ')
       end
-      printf "%-20s  %-2s  %s\n", player.search('a').inner_html, points, sets.join(' ')
+      print "\n"
     end
-    print "\n"
   end
 end
+
+output_page("http://wimbledon.com/en_GB/scores/index2.html")
+puts "---- Completed ----"
+day = find_day
+output_page("http://www.wimbledon.com/en_GB/scores/results/#{day}.html")
