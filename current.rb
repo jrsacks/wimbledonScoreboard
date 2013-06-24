281cff080fc158615504895f230a6a1017e30489
Jeff Sacks <jrsacks@gmail.com>
2013-06-24 09:27:33 -0500

    updating for 2013 wimbledon website changes

--- current.rb
+++ current.rb
@@ -7,10 +7,10 @@ def find_day
   open("http://www.wimbledon.com/en_GB/scores/results/index.html").read.match(/day[0-9]*/)[0]
 end
 
-def output_page(url)
+def output_page(url, searchClass)
   doc = Nokogiri::HTML(open(url))
   doc.css('.scoringtable').each do |div|
-    if div.css('.eventinfo').first.inner_html.include? "Gentlemen's Singles"
+    if div.css(searchClass).first.inner_html.include? "Gentlemen's Singles"
       div.css('.crtrow').each do |player|
         server = ''
         server = '-' if player.css('.currentServer').length > 0
@@ -26,6 +26,6 @@ def output_page(url)
 end
 
 puts "---- Live ----"
-output_page("http://wimbledon.com/en_GB/scores/index2.html")
+output_page("http://wimbledon.com/en_GB/scores/index2.html", '.eventinfo')
 puts "---- Completed ----"
-output_page("http://www.wimbledon.com/en_GB/scores/results/#{find_day}.html")
+output_page("http://www.wimbledon.com/en_GB/scores/results/#{find_day}.html", '.courtname')
